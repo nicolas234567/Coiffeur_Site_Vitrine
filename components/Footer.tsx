daf20dcf596c8441e09264a7b890bf77a5ff62c0
Nicolas Bregevin <nicolasbregevin878@gmail.com>
2026-03-01 20:18:48 +0100

    ajout d'image et de texte

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,14 +2,14 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer_line">
-        <span className="left">12 rue des Lilas, 75011 Paris</span>
-        <span className="center">Élise Coiffure Votre beauté, notre passion depuis 2005 </span>
-        <span className="right">Contact: contact@elisecoiffeur.fr</span>
+        <span className="footer_left">12 rue des Lilas, 75011 Paris</span>
+        <span className="footer_center">Élise Coiffure Votre beauté, notre passion depuis 2005 </span>
+        <span className="footer_right">Contact: contact@elisecoiffeur.fr</span>
       </div>
       <div className="footer_line_small">
-        <span className="left">Tél : 01 23 45 67 89</span>
-        <span className="middle">&copy; 2024 Salon Élise. Tous droits réservés.</span>
-        <span className="right">Crée par :<a href="https://nicolasbregevin.netlify.app/">Nicolas BREGEVIN</a></span>
+        <span className="footer_left">Tél : 01 23 45 67 89</span>
+        <span className="footer_center">&copy; 2024 Salon Élise. Tous droits réservés.</span>
+        <span className="footer_right">Crée par :<a href="https://nicolasbregevin.netlify.app/">Nicolas BREGEVIN</a></span>
       </div>
     </footer>
   );}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,49 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <main className="hero">
-        <h1>Salon de coiffure Professionnel pour femme</h1>
-        <p>
-          "Bienvenue au Salon Élise, votre salon de coiffure à Soisy-sur-seine. Notre équipe de professionnels vous accueille dans un cadre chaleureux pour sublimer votre style. Coupe, coloration, soin — nous prenons soin de vous."
-        </p>
+        <h1>Elise coiffure - Salon de coiffure Professionnel pour femme</h1>
+        <h2>
+          Bienvenue au Salon Élise, votre salon de coiffure à Soisy-sur-seine. 
+          <br />Notre équipe de professionnels vous accueille dans un cadre chaleureux pour sublimer votre style.
+          <br />Coupe, coloration, soin — nous prenons soin de vous.
+        </h2>
+        <div className="images_left">
+          <Image
+              src="/images/coiffure_laque.jpg"
+              alt="coiffure_laque"
+              width={800}
+              height={600}
+              className="hero_image"
+            />
+        </div>
+        <span className="images_right">
+          <span className="texte_left">
+            Nos tarifs :<br /><br />
+
+            Coupe femme : 30€<br />
+            Coupe homme : 22€<br />
+            Coupe enfant (-12 ans) : 18€<br /><br />
+
+            Coloration racines : 50€<br />
+            Coloration complète : 65€<br />
+            Balayage / mèches : 75€<br /><br />
+
+            Soin nourrissant : 20€<br />
+            Soin profond réparateur : 35€<br />
+            Soin bot capillaire : 60€<br /><br />
+
+            Coiffure événement (mariage, soirée) : à partir de 80€<br />
+            Forfait mariée (essai + jour J) : 150€<br /><br />
+          </span>
+          <Image
+              src="/images/coiffure_carré.jpg"
+              alt="coiffure_carré"
+              width={600}
+              height={200}
+              className="hero_image"
+            />
+        </span>
     </main>
   );}
